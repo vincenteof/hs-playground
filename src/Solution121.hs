4ba99ac05b875e3171a27979c692f50c08d1544e
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-04-12 08:48:15 +0800

    feat: 121

---
+++ src/Solution121.hs
@@ -0,0 +1,17 @@
+module Solution121 where
+
+-- https://leetcode.cn/problems/best-time-to-buy-and-sell-stock
+
+maxPriceBound :: Int
+maxPriceBound = 10 * 10 * 10 * 10 + 1
+
+maxProfit :: [Int] -> Int
+maxProfit prices = maxProfitHelper prices maxPriceBound minBound
+
+maxProfitHelper :: [Int] -> Int -> Int -> Int
+maxProfitHelper [] _ res = res
+maxProfitHelper (p : ps) buyPrice res =
+  maxProfitHelper ps newBuyPrice newRes
+  where
+    newBuyPrice = min buyPrice p
+    newRes = max res (p - newBuyPrice)
--- test/Test.hs
+++ test/Test.hs
@@ -1,18 +1,19 @@
 module Main where
-import qualified LeetCode 
-import Test.HUnit
+
+import qualified Solution121
 import qualified System.Exit as Exit
-
-
+import Test.HUnit
 
 test1 :: Test
-test1 = TestCase (assertEqual "should return 3" 3 (LeetCode.plus 1 2))
+test1 = TestCase (assertEqual "basic case" 5 (Solution121.maxProfit [7, 1, 5, 3, 6, 4]))
+
+test2 :: Test
+test2 = TestCase (assertEqual "no transaction" 0 (Solution121.maxProfit [7, 6, 4, 3, 1]))
 
 tests :: Test
-tests = TestList [TestLabel "test1" test1]
-
+tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
 
 main :: IO ()
 main = do
-    result <- runTestTT tests
-    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess   
+  result <- runTestTT tests
+  if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
